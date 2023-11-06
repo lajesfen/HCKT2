4742ba7247e5da19457fbb6fd9409382ff90b0d9
stefano565-Utec <142551373+stefano565-Utec@users.noreply.github.com>
2023-11-06 17:17:07 -0500

    Add files via upload

---
+++ src/main/java/me/dbp/api/repository/TypeGroupRepository.java
@@ -0,0 +1,8 @@
+package me.dbp.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import me.dbp.api.entity.TypeGroup;
+
+public interface TypeGroupRepository extends JpaRepository<TypeGroup,Long>{
+    
+}
