4709396a1d24b85efff16ffb9df24be48eb207df
Sofia <sofia.salazar.m@utec.edu.pe>
2023-11-06 17:26:32 -0500

    update

--- src/main/java/me/dbp/api/controller/TypeGroupController.java
+++ src/main/java/me/dbp/api/controller/TypeGroupController.java
@@ -1,16 +1,15 @@
 package me.dbp.api.controller;
+import me.dbp.api.entity.Group;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import me.dbp.api.entity.TypeGroup;
 import me.dbp.api.repository.TypeGroupRepository;
 
+import java.lang.reflect.Type;
 import java.security.PublicKey;
 import java.util.List;
 
@@ -29,12 +28,27 @@ public class TypeGroupController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getTypeGroup(@PathVariable Long id){
-
         if(!typeGroupRepository.existsById(id)){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("TypeGroup with ID " + id + " not found");
         }
         
-        TypeGroup exisTypeGroup = typeGroupRepository.findById(id).get();
+        TypeGroup existingTypeGroup = typeGroupRepository.findById(id).get();
+        return new ResponseEntity<>(existingTypeGroup, HttpStatus.OK);
+    }
+    
+    @PostMapping
+    public ResponseEntity<String> typeGroup(@RequestBody TypeGroup typeGroup) {
+        typeGroupRepository.save(typeGroup);
+        return ResponseEntity.status(201).body("Created");
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteGroup5(@PathVariable Long id) {
+        if (!typeGroupRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Group with ID " + id + " not found");
+        }
+
+        typeGroupRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Deleted");
+    }
 }
--- src/main/java/me/dbp/api/repository/TypeGroupRepository.java
+++ src/main/java/me/dbp/api/repository/TypeGroupRepository.java
@@ -3,6 +3,6 @@ package me.dbp.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import me.dbp.api.entity.TypeGroup;
 
-public interface TypeGroupRepository extends JpaRepository<TypeGroup,Long>{
-    
+public interface TypeGroupRepository extends JpaRepository<TypeGroup,Long> {
+
 }
